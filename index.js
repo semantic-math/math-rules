f7e2658d98f94eafbaad0279d340ff2a4b365e54
Kevin Barabash <kevinb@khanacademy.org>
2017-05-14 12:06:08 -0400

    initial publish

---
+++ index.js
@@ -0,0 +1,9 @@
+import {defineRule, canApplyRule, applyRule} from './lib/matcher'
+import * as rules from './lib/rules.js'
+
+export {
+    defineRule,
+    canApplyRule,
+    applyRule,
+    rules,
+}
