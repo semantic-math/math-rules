648f0f761a40beeae7d64d4840c6f22aebcc8ca3
Anthony <aliang8@stuy.edu>
2017-05-20 11:19:54 -0400

    remove alphabetize

--- test/rules_test.js
+++ test/rules_test.js
@@ -206,6 +206,8 @@ describe('rules', () => {
         ['2x + 7y + 5 + 3y + 9x + 11', '(2 x + 9 x) + (7 y + 3 y) + (5 + 11)'],
     ])
 
+    // ADDING POLYNOMIALS
+
     suite('add polynomials', rules.ADD_POLYNOMIAL_TERMS, [
         ['2x + 2x + 2 + 4', '4 x + (2 + 4)'],
         ['3y^2 - 2y^2 + y^4', '1 y^2 + 1 y^4'],
@@ -214,7 +216,7 @@ describe('rules', () => {
         ['-2y + 3y', '1 y'],
         ['3 xy + 2 xy', '5 xy'],
         ['3 xy - 2 xy + x^2y^2', '1 (x^2 y^2) + 1 xy'],
-        //['2xy + 2yx', '4 xy'],
+        ['2 x y + 2 y x', '4 (x y)'],
     ])
 
     suite('handles basic arithmetic', rules.SIMPLIFY_ARITHMETIC, [
